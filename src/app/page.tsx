e0ff66515daff6426f7a3a678f49bf3dc45a4894
fnmatheus <nasc.matheusfrancisco@gmail.com>
2024-04-01 09:24:50 -0300

    feat: change sheets to only one document

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,7 @@ import { useEffect, useRef, useState } from 'react';
 import { IHardSkillsList, IProject } from './utils/interfaces';
 import Loading from './loading';
 
-const projectsUrl = 'https://script.google.com/macros/s/AKfycbwwttTjdDna2bMQaJY4HaGFzcCgxu6vN8-EbQBxqX8ZQzDGIWtSeJvTLDbDTlO4BjPVYg/exec';
-const hardskillsUrl = 'https://script.google.com/macros/s/AKfycbzorQBBFngf4LR645uShRF_eUkI_825_sfTcRhRBGz5_ftTURDjTeDmq62rjLM4grsZ/exec';
+const getDataUrl = 'https://script.google.com/macros/s/AKfycbzazCxicyfNtMoScUVEBxB3SZOv2RFMwkFjGVyZj9jaidI1g-4WTt27BsjDvds7eupwag/exec';
 const nullHardSkills = {skills: [], studies: [], certificates: []}
 
 export default function Home() {
@@ -29,12 +28,12 @@ export default function Home() {
   useEffect(() => {
     const getData = async () => {
       try {
-        const projectsResponse = await fetch(projectsUrl, {method: 'GET'});
-        const projectsData = await projectsResponse.json();
-        const hardskillsResponse = await fetch(hardskillsUrl, {method: 'GET'});
-        const hardskillsData = await hardskillsResponse.json();
-        await setProjectsArr(projectsData);
-        await setHardskillsObj(hardskillsData);
+        const response = await fetch(getDataUrl, {method: 'GET'});
+        const data = await response.json();
+        const {projects, hardSkills, images} = data;
+        await setProjectsArr(projects);
+        await setHardskillsObj(hardSkills);
+        console.log(projects)
         setIsLoading(false);
       } catch (error) {
         console.log('error');
